fcb6dc3a5af253c44666bffd67010473eb380967
Impana-S-G <impanas1999@gmail.com>
2021-08-20 20:38:27 +0530

    This is maven project

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class HelloWorld extends WebPage {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public HelloWorld() {
+		super();
+	}
+
+}
